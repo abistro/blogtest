e5cd33f16ad09a638ad3f97b4c96bc8941b5d95a
Van Thai <asian.bistro@gmail.com>
2016-10-01 21:04:16 -0400

    Added 'add btn' and functionality

--- scripts.js
+++ scripts.js
@@ -37,7 +37,11 @@ var BlogView = Backbone.View.extend({
 	model: new Blog(),
 	tagName: 'tr',
 	initialize: function() {
-		this.template = _.template($('.blogs-list-template').html()
+		this.template = _.template($('.blogs-list-template').html());
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
 	}
 
 });
@@ -45,6 +49,39 @@ var BlogView = Backbone.View.extend({
 // Backbone views for all blog
 
 var BlogsView = Backbone.View.extend({
-
+	model: blogs,
+	el: $('.blogs-list'),
+	initialize: function() {
+		this.model.on('add', this.render, this);
+	},
+	render: function() {
+		var self = this;
+		this.$el.html('');
+		_.each(this.model.toArray(), function(blog) {
+			self.$el.append((new BlogView({model: blog})).render().$el);
+		})
+		return this;
+	}
 });
 
+var blogsView = new BlogsView();
+
+$(document).ready(function() {
+	// After document ready, setup on click event on the add blog btn
+	$('.add-blog').on('click', function(){
+		var blog = new Blog({
+			author: $('.author-input').val(),
+			title: $('.title-input').val(),
+			url: $('.url-input').val()
+		});
+
+		// Clear out input boxes after 'on click' event
+		$('.author-input').val('');
+		$('.title-input').val('');
+		$('.url-input').val('');
+
+		// output blog model to JSON
+		console.log(blog.toJSON());
+		blogs.add(blog);
+	});
+});
